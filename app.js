35f77a747aa70e9353a819061357e44a9eda952f
jorj-kare <jorj.kare@gmail.com>
2022-11-29 22:23:31 +0100

    feat: add routes for posts

--- app.js
+++ app.js
@@ -1,4 +1,8 @@
 const express = require("express");
 const app = express();
+const postRouter = require("./routes/postRoutes");
+
+app.use(express.json());
+app.use("/api/v1/posts", postRouter);
 
 module.exports = app;
