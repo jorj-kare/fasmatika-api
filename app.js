a65f3cab9a447f5c6c36bdb023b63ccd89820269
jorj-kare <jorj.kare@gmail.com>
2022-12-06 21:45:36 +0100

    add cors headers

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 const express = require("express");
+const cors = require("cors");
 const app = express();
 const postRouter = require("./routes/postRoutes");
-
+app.use(cors());
 app.use(express.json());
 app.use("/api/v1/posts", postRouter);
 
