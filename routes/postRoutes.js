88ef1f6e71878d87f9e2b3853df274f2dfc76205
jorj-kare <jorj.kare@gmail.com>
2022-12-06 21:58:39 +0100

    add middleware to upload images

--- routes/postRoutes.js
+++ routes/postRoutes.js
@@ -3,7 +3,10 @@ const postController = require("../controllers/postController");
 
 const router = express.Router();
 
-router.route("/").get(postController.getPosts).post(postController.createPost);
+router
+  .route("/")
+  .get(postController.getPosts)
+  .post(postController.uploadPostImg, postController.createPost);
 router
   .route("/:id")
   .patch(postController.updatePost)
