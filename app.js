7968d548075abcc9bd24f670c94ac20c50efc4a7
jorj-kare <jorj.kare@gmail.com>
2023-01-04 18:28:18 +0100

    add user router add widdleware

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
 const express = require("express");
 const cors = require("cors");
+const cookieParser = require("cookie-parser");
+const helmet = require("helmet");
+
 const app = express();
 const postRouter = require("./routes/postRoutes");
 app.use(cors());
 app.use(express.json());
+app.use(cookieParser());
 app.use("/api/v1/posts", postRouter);
+app.use("/api/v1/user", userRouter);
 
 module.exports = app;
