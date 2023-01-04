fea7d24572f3b8d67644588748cfe42ac136f577
jorj-kare <jorj.kare@gmail.com>
2023-01-04 18:29:57 +0100

    add helmet middleware

--- app.js
+++ app.js
@@ -5,7 +5,30 @@ const helmet = require("helmet");
 
 const app = express();
 const postRouter = require("./routes/postRoutes");
-app.use(cors());
+const userRouter = require("./routes/userRoutes");
+
+// app.use(cors());
+app.use(helmet());
+// const scriptSrcUrls = [];
+// const styleSrcUrls = [];
+// const connectSrcUrls = [];
+// const fontSrcUrls = [];
+// const imgSource = [];
+
+// app.use(
+//   helmet.contentSecurityPolicy({
+//     directives: {
+//       defaultSrc: [],
+//       connectSrc: ["'self'", ...connectSrcUrls],
+//       scriptSrc: ["'self'", ...scriptSrcUrls],
+//       styleSrc: ["'self'", "'unsafe-inline'", ...styleSrcUrls],
+//       workerSrc: ["'self'", "blob:"],
+//       objectSrc: [],
+//       imgSrc: ["'self'", "blob:", "data:", ...imgSource],
+//       fontSrc: ["'self'", ...fontSrcUrls],
+//     },
+//   })
+// );https://github.com/sutin1234/sveltekit-mongodb/blob/main/src/lib/counter/counter.spec.tsrail
 app.use(express.json());
 app.use(cookieParser());
 app.use("/api/v1/posts", postRouter);
